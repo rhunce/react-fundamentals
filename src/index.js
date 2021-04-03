3fd761d22e74da76f97f9b2518c3d17ee6a72f7a
Raymond Hunce <hunce18c@gmail.com>
2021-04-03 14:59:44 -0400

    Create list of two books

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firstBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/81YzHKeWq7L._AC_UL200_SR200,200_.jpg',
+  title: 'The Midnight Library: A Novel',
+  author: 'Matt Haig',
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg',
+  title: 'The Four Agreements',
+  author: 'Dom Miguel Ruiz',
+};
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        reprehenderit numquam reiciendis non fuga qui ullam placeat architecto
+        tempore incidunt.
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
+  );
+}
+
+function Book({ img, title, author, children }) {
+  return (
+    <article className='book'>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p>{children}</p>
+    </article>
+  );
+}
+
+ReactDom.render(<BookList />, document.getElementById('root'));
